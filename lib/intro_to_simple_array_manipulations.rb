fc409e55b7c45403a528231ad28c98b2565433e6
Jeremy Gray <jeremygray@utexas.edu>
2019-11-09 20:40:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,18 @@
 def using_push(array,string)
   array.push(string)
 end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def usiing_pop(array)
+  array.pop
+end
+
+def shift_with_args(array)
+  arg1=array.shift
+  arg2=array.shift
+  [arg1,arg2]
+end
+  
