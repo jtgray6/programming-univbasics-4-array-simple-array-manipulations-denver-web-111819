36e9ae3a34b7176edf397d1795d5584f38f300d4
Jeremy Gray <jeremygray@utexas.edu>
2019-11-09 20:47:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,4 +25,11 @@ def shift_with_args(array)
   arg2=array.shift
   [arg1,arg2]
 end
-  
+
+def using_concat(array1,array2)
+  array1.concat(array2)
+end
+
+def using_insert(array,element)
+  array.insert(3)=element
+end  
